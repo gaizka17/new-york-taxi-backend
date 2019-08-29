b89771d1e3d8dd7fc3cf27e90f93a13fe283fd87
Arizmendiarrieta Marina <gaizka.arizmendiarrieta@tecnalia.com>
2019-08-29 17:04:03 +0200

    Add more vars to mongo

--- rest.py
+++ rest.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 import pymongo
 import pandas as pd
 import json
+import numpy as np
 
 from functools import wraps
 app = Flask(__name__)
@@ -64,7 +65,13 @@ def process_file(name):
     column_names = df.columns[2:]
     df = df.iloc[:, :-2]
     df.columns = column_names
-    df = df[['Passenger_count', 'Trip_distance', 'Fare_amount', 'Total_amount']]
+    df = df[['Passenger_count', 'Trip_distance', 'Fare_amount', 'Total_amount','lpep_pickup_datetime','Lpep_dropoff_datetime']]
+    #datetime_1 = datetime.strptime(df['Lpep_dropoff_datetime'], '%y-%m-%d %H:%M:%S')
+    #datetime_2 = datetime.strptime(df['lpep_pickup_datetime'], '%y-%m-%d %H:%M:%S')
+    #print((datetime_1 - datetime_2).datetime.minutes)
+    df['Lpep_dropoff_datetime'] = pd.to_datetime(df['Lpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S')
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'], format='%Y-%m-%d %H:%M:%S')
+    df['Total_time'] = df['Lpep_dropoff_datetime'] - df['lpep_pickup_datetime']
     test = df.describe()
     test2 = test['Passenger_count']
     res = {}
@@ -73,28 +80,47 @@ def process_file(name):
     res['passenger_std'] = test2[2]
     res['passenger_min'] = test2[3]
     res['passenger_max'] = test2[7]
+    res['passenger_25'] = test2[4]
+    res['passenger_50'] = test2[5]
+    res['passenger_75'] = test2[6]
     test2 = test['Trip_distance']
     res['Trip_distance_avg'] = test2[1]
     res['Trip_distance_std'] = test2[2]
     res['Trip_distance_min'] = test2[3]
     res['Trip_distance_max'] = test2[7]
+    res['Trip_distance_25'] = test2[4]
+    res['Trip_distance_50'] = test2[5]
+    res['Trip_distance_75'] = test2[6]
     test2 = test['Fare_amount']
     res['Fare_amount_avg'] = test2[1]
     res['Fare_amount_std'] = test2[2]
     res['Fare_amount_min'] = test2[3]
     res['Fare_amount_max'] = test2[7]
+    res['Fare_amount_25'] = test2[4]
+    res['Fare_amount_50'] = test2[5]
+    res['Fare_amount_75'] = test2[6]
     test2 = test['Total_amount']
     res['Total_amount_avg'] = test2[1]
     res['Total_amount_std'] = test2[2]
     res['Total_amount_min'] = test2[3]
     res['Total_amount_max'] = test2[7]
+    res['Total_amount_25'] = test2[4]
+    res['Total_amount_50'] = test2[5]
+    res['Total_amount_75'] = test2[6]
+    test2 = test['Total_time']
+    res['Total_time_avg'] = test2[1] / np.timedelta64(1, 'm')
+    res['Total_time_std'] = test2[2] / np.timedelta64(1, 'm')
+    res['Total_time_min'] = test2[3] / np.timedelta64(1, 'm')
+    res['Total_time_max'] = test2[7] / np.timedelta64(1, 'm')
+    res['Total_time_25'] = test2[4] / np.timedelta64(1, 'm')
+    res['Total_time_50'] = test2[5] / np.timedelta64(1, 'm')
+    res['Total_time_75'] = test2[6] / np.timedelta64(1, 'm')
     res['car_type'] = name[0]
     fecha = name[2].split('-')
     a = datetime(int(fecha[0]),int(fecha[1]),1)
     a = datetime.timestamp(a)
     res['timestamp'] = a
-    print(res)
-    print(type(res))
+    #print(res)
 
     client = get_mongo_client()
     # Issue the serverStatus command and print the results
