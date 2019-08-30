79caa0bea4387dbfb8137d7d58b5cebdd0cbc748
Arizmendiarrieta Marina <gaizka.arizmendiarrieta@tecnalia.com>
2019-08-30 17:47:02 +0200

    Added some improvements

--- rest.py
+++ rest.py
@@ -59,21 +59,123 @@ def do_login_pla():
 @app.route('/file/<name>')
 @login_required
 def process_file(name):
-    df = pd.read_csv(r"C:\Users\109366\Desktop\TFM codigo\data\\"+name, sep=",",
+    if "yellow" in name:
+        print("Parece que se va tratar el amarillo")
+        yellow_file(name)
+    elif "green" in name:
+        print("Parece que se va tratar el verde")
+        green_file(name)
+    elif "fhv" in name:
+        print("Parece que se va tratar el fhv")
+        fhv_file(name)
+
+    return jsonify("Saved")
+
+@app.route('/now/<date>')
+@login_required
+def now(date):
+    test = [date[i:i+4] for i in range(0, len(date), 4)]
+    test2 = [date[i:i + 2] for i in range(0, len(date), 2)]
+    fecha = datetime(int(test[0]), int(test2[2]), 1).timestamp()
+    client = get_mongo_client()
+    # Issue the serverStatus command and print the results
+    mydb = client["NEW_YORK_TAXI"]
+    mycol = mydb["month"]
+    cursor = mycol.find({"timestamp": fecha, "car_type": "green"})
+    res = {}
+    for doc in cursor:
+        res['green'] = doc
+        res['green'].pop('_id', None)
+        
+    cursor = mycol.find({"timestamp": fecha, "car_type": "yellow"})
+    for doc in cursor:
+        res['yellow'] = doc
+        res['yellow'].pop('_id', None)
+        #respuesta = respuesta.append(res)
+    return jsonify(res)
+
+@app.route("/trace/<date>/")
+@login_required
+def get_programs_rest(date):
+    test = [date[i:i + 4] for i in range(0, len(date), 4)]
+    desde = datetime(int(test[0]), 1, 1).timestamp()
+    hasta = datetime(int(test[0])+1, 1, 1).timestamp()
+    vars = request.args.get('vars')
+    var_list = vars.split(',')
+
+    client = get_mongo_client()
+    # Issue the serverStatus command and print the results
+    mydb = client["NEW_YORK_TAXI"]
+    mycol = mydb["month"]
+    # res = {}
+    # for doc in cursor:
+    #     res['green'] = doc
+    #     res['green'].pop('_id', None)
+    #     # respuesta = respuesta.append(res)
+    # return jsonify(res)
+    data_json = []
+    i = 1
+    axis = {}
+
+    for variable in var_list:
+        cursor = mycol.find({'timestamp': {'$gte': desde, '$lte': hasta }},{variable: 1, 'timestamp': 1, "_id": 0}).sort("timestamp",pymongo.DESCENDING)
+        previous_ts= 0
+        res = []
+        for doc in cursor:
+            resp = doc
+            if previous_ts==0:
+                #data = [0, float(resp[variable])]
+                try:
+                    data = [resp['timestamp']*1000, float(resp[variable])]
+                    previous_ts= resp['timestamp']
+                except:
+                    continue
+            else:
+                #data = [resp['start']-previous_ts, float(resp[variable])]
+                try:
+                    data = [resp['timestamp']*1000, float(resp[variable])]
+                except:
+                    continue
+
+            #resp.pop('_id', None)
+            res.append(data)
+
+        # print res
+        res_json = {}
+        res_json['data'] = res
+        res_json['label'] = variable
+        data = variable.split("_")
+        if data[1] in axis:
+            res_json['yaxis'] = axis[data[1]]
+        else:
+            axis[data[1]] = i
+            res_json['yaxis'] = i
+            i = i + 1
+
+        data_json.append(res_json)
+
+    return jsonify(data_json)
+def fhv_file(name):
+    df = pd.read_csv(r"C:\Users\109366\Desktop\TFM codigo\data\\fhv\\"+name, sep=",",
                      index_col=None).reset_index()
     name = name.replace('.csv','').split('_')
-    column_names = df.columns[2:]
-    df = df.iloc[:, :-2]
-    df.columns = column_names
-    df = df[['Passenger_count', 'Trip_distance', 'Fare_amount', 'Total_amount','lpep_pickup_datetime','Lpep_dropoff_datetime']]
-    #datetime_1 = datetime.strptime(df['Lpep_dropoff_datetime'], '%y-%m-%d %H:%M:%S')
-    #datetime_2 = datetime.strptime(df['lpep_pickup_datetime'], '%y-%m-%d %H:%M:%S')
-    #print((datetime_1 - datetime_2).datetime.minutes)
-    df['Lpep_dropoff_datetime'] = pd.to_datetime(df['Lpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S')
-    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'], format='%Y-%m-%d %H:%M:%S')
+    # column_names = df.columns[2:] #old files of green taxi
+    # df = df.iloc[:, :-2] #old files of green taxi
+    # df.columns = column_names #old files of green taxi
+    #df = df[['Passenger_count', 'Trip_distance', 'Fare_amount', 'Total_amount','lpep_pickup_datetime','Lpep_dropoff_datetime']] #old files of green taxi
+    #df = df[['passenger_count', 'trip_distance', 'fare_amount', 'total_amount', 'lpep_pickup_datetime',
+    #          'lpep_dropoff_datetime']]
+    df = df[['passenger_count', 'trip_distance', 'fare_amount', 'total_amount', 'tpep_pickup_datetime',
+             'tpep_dropoff_datetime']] #para los amarillos
+    #df['Lpep_dropoff_datetime'] = pd.to_datetime(df['Lpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S') #old files of green taxi
+    #df['Lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S')
+    df['Lpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S')  #para los amarillos
+    #df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'], format='%Y-%m-%d %H:%M:%S')
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'], format='%Y-%m-%d %H:%M:%S') # para los amarilos
     df['Total_time'] = df['Lpep_dropoff_datetime'] - df['lpep_pickup_datetime']
     test = df.describe()
-    test2 = test['Passenger_count']
+    #test2 = test['Passenger_count'] #old files of green taxi
+    test2 = test['passenger_count']
     res = {}
     res['total_trips'] = test2[0]
     res['passenger_avg'] = test2[1]
@@ -83,7 +185,8 @@ def process_file(name):
     res['passenger_25'] = test2[4]
     res['passenger_50'] = test2[5]
     res['passenger_75'] = test2[6]
-    test2 = test['Trip_distance']
+    #test2 = test['Trip_distance'] #old files of green taxi
+    test2 = test['trip_distance']
     res['Trip_distance_avg'] = test2[1]
     res['Trip_distance_std'] = test2[2]
     res['Trip_distance_min'] = test2[3]
@@ -91,7 +194,8 @@ def process_file(name):
     res['Trip_distance_25'] = test2[4]
     res['Trip_distance_50'] = test2[5]
     res['Trip_distance_75'] = test2[6]
-    test2 = test['Fare_amount']
+    #test2 = test['Fare_amount'] #old files of green taxi
+    test2 = test['fare_amount']
     res['Fare_amount_avg'] = test2[1]
     res['Fare_amount_std'] = test2[2]
     res['Fare_amount_min'] = test2[3]
@@ -99,7 +203,8 @@ def process_file(name):
     res['Fare_amount_25'] = test2[4]
     res['Fare_amount_50'] = test2[5]
     res['Fare_amount_75'] = test2[6]
-    test2 = test['Total_amount']
+    #test2 = test['Total_amount'] #old files of green taxi
+    test2 = test['total_amount']
     res['Total_amount_avg'] = test2[1]
     res['Total_amount_std'] = test2[2]
     res['Total_amount_min'] = test2[3]
@@ -120,23 +225,162 @@ def process_file(name):
     a = datetime(int(fecha[0]),int(fecha[1]),1)
     a = datetime.timestamp(a)
     res['timestamp'] = a
-    #print(res)
 
     client = get_mongo_client()
     # Issue the serverStatus command and print the results
     mydb = client["NEW_YORK_TAXI"]
     mycol = mydb["month"]
     mycol.insert_one(res)
-    #cursor = mycol.find()
-    # cursor = mycol.find({"machine_part": "status"}).limit(1).sort("start",pymongo.DESCENDING)
-    # for doc in cursor:
-    #     res['status'] = doc
-    #     res['status'].pop('_id', None)
-    #     #respuesta = respuesta.append(res)
-    #print respuesta
+def green_file(name):
+    df = pd.read_csv(r"C:\Users\109366\Desktop\TFM codigo\data\\green\\"+name, sep=",",
+                     index_col=None).reset_index()
+    name = name.replace('.csv','').split('_')
+    # column_names = df.columns[2:] #old files of green taxi
+    # df = df.iloc[:, :-2] #old files of green taxi
+    # df.columns = column_names #old files of green taxi
+    #df = df[['Passenger_count', 'Trip_distance', 'Fare_amount', 'Total_amount','lpep_pickup_datetime','Lpep_dropoff_datetime']] #old files of green taxi
+    df = df[['passenger_count', 'trip_distance', 'fare_amount', 'total_amount', 'lpep_pickup_datetime',
+             'lpep_dropoff_datetime']]
+
+    #df['Lpep_dropoff_datetime'] = pd.to_datetime(df['Lpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S') #old files of green taxi
+    df['Lpep_dropoff_datetime'] = pd.to_datetime(df['lpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S')
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'], format='%Y-%m-%d %H:%M:%S')
+    df['Total_time'] = df['Lpep_dropoff_datetime'] - df['lpep_pickup_datetime']
+    test = df.describe()
+    #test2 = test['Passenger_count'] #old files of green taxi
+    test2 = test['passenger_count']
+    res = {}
+    res['total_trips'] = test2[0]
+    res['passenger_avg'] = test2[1]
+    res['passenger_std'] = test2[2]
+    res['passenger_min'] = test2[3]
+    res['passenger_max'] = test2[7]
+    res['passenger_25'] = test2[4]
+    res['passenger_50'] = test2[5]
+    res['passenger_75'] = test2[6]
+    #test2 = test['Trip_distance'] #old files of green taxi
+    test2 = test['trip_distance']
+    res['Trip_distance_avg'] = test2[1]
+    res['Trip_distance_std'] = test2[2]
+    res['Trip_distance_min'] = test2[3]
+    res['Trip_distance_max'] = test2[7]
+    res['Trip_distance_25'] = test2[4]
+    res['Trip_distance_50'] = test2[5]
+    res['Trip_distance_75'] = test2[6]
+    #test2 = test['Fare_amount'] #old files of green taxi
+    test2 = test['fare_amount']
+    res['Fare_amount_avg'] = test2[1]
+    res['Fare_amount_std'] = test2[2]
+    res['Fare_amount_min'] = test2[3]
+    res['Fare_amount_max'] = test2[7]
+    res['Fare_amount_25'] = test2[4]
+    res['Fare_amount_50'] = test2[5]
+    res['Fare_amount_75'] = test2[6]
+    #test2 = test['Total_amount'] #old files of green taxi
+    test2 = test['total_amount']
+    res['Total_amount_avg'] = test2[1]
+    res['Total_amount_std'] = test2[2]
+    res['Total_amount_min'] = test2[3]
+    res['Total_amount_max'] = test2[7]
+    res['Total_amount_25'] = test2[4]
+    res['Total_amount_50'] = test2[5]
+    res['Total_amount_75'] = test2[6]
+    test2 = test['Total_time']
+    res['Total_time_avg'] = test2[1] / np.timedelta64(1, 'm')
+    res['Total_time_std'] = test2[2] / np.timedelta64(1, 'm')
+    res['Total_time_min'] = test2[3] / np.timedelta64(1, 'm')
+    res['Total_time_max'] = test2[7] / np.timedelta64(1, 'm')
+    res['Total_time_25'] = test2[4] / np.timedelta64(1, 'm')
+    res['Total_time_50'] = test2[5] / np.timedelta64(1, 'm')
+    res['Total_time_75'] = test2[6] / np.timedelta64(1, 'm')
+    res['car_type'] = name[0]
+    fecha = name[2].split('-')
+    a = datetime(int(fecha[0]),int(fecha[1]),1)
+    a = datetime.timestamp(a)
+    res['timestamp'] = a
+
+    client = get_mongo_client()
+    # Issue the serverStatus command and print the results
+    mydb = client["NEW_YORK_TAXI"]
+    mycol = mydb["month"]
+    mycol.insert_one(res)
+
+def yellow_file(name):
+    df = pd.read_csv(r"C:\Users\109366\Desktop\TFM codigo\data\\yellow\\" + name, sep=",",
+                     index_col=None).reset_index()
+    name = name.replace('.csv', '').split('_')
+    # column_names = df.columns[2:] #old files of green taxi
+    # df = df.iloc[:, :-2] #old files of green taxi
+    # df.columns = column_names #old files of green taxi
+    # df = df[['Passenger_count', 'Trip_distance', 'Fare_amount', 'Total_amount','lpep_pickup_datetime','Lpep_dropoff_datetime']] #old files of green taxi
+    df = df[['passenger_count', 'trip_distance', 'fare_amount', 'total_amount', 'tpep_pickup_datetime',
+             'tpep_dropoff_datetime']]  # para los amarillos
+    # df['Lpep_dropoff_datetime'] = pd.to_datetime(df['Lpep_dropoff_datetime'], format='%Y-%m-%d %H:%M:%S') #old files of green taxi
+    df['Lpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'],
+                                                 format='%Y-%m-%d %H:%M:%S')  # para los amarillos
+    df['lpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'],
+                                                format='%Y-%m-%d %H:%M:%S')  # para los amarilos
+    df['Total_time'] = df['Lpep_dropoff_datetime'] - df['lpep_pickup_datetime']
+    test = df.describe()
+    # test2 = test['Passenger_count'] #old files of green taxi
+    test2 = test['passenger_count']
+    res = {}
+    res['total_trips'] = test2[0]
+    res['passenger_avg'] = test2[1]
+    res['passenger_std'] = test2[2]
+    res['passenger_min'] = test2[3]
+    res['passenger_max'] = test2[7]
+    res['passenger_25'] = test2[4]
+    res['passenger_50'] = test2[5]
+    res['passenger_75'] = test2[6]
+    # test2 = test['Trip_distance'] #old files of green taxi
+    test2 = test['trip_distance']
+    res['Trip_distance_avg'] = test2[1]
+    res['Trip_distance_std'] = test2[2]
+    res['Trip_distance_min'] = test2[3]
+    res['Trip_distance_max'] = test2[7]
+    res['Trip_distance_25'] = test2[4]
+    res['Trip_distance_50'] = test2[5]
+    res['Trip_distance_75'] = test2[6]
+    # test2 = test['Fare_amount'] #old files of green taxi
+    test2 = test['fare_amount']
+    res['Fare_amount_avg'] = test2[1]
+    res['Fare_amount_std'] = test2[2]
+    res['Fare_amount_min'] = test2[3]
+    res['Fare_amount_max'] = test2[7]
+    res['Fare_amount_25'] = test2[4]
+    res['Fare_amount_50'] = test2[5]
+    res['Fare_amount_75'] = test2[6]
+    # test2 = test['Total_amount'] #old files of green taxi
+    test2 = test['total_amount']
+    res['Total_amount_avg'] = test2[1]
+    res['Total_amount_std'] = test2[2]
+    res['Total_amount_min'] = test2[3]
+    res['Total_amount_max'] = test2[7]
+    res['Total_amount_25'] = test2[4]
+    res['Total_amount_50'] = test2[5]
+    res['Total_amount_75'] = test2[6]
+    test2 = test['Total_time']
+    res['Total_time_avg'] = test2[1] / np.timedelta64(1, 'm')
+    res['Total_time_std'] = test2[2] / np.timedelta64(1, 'm')
+    res['Total_time_min'] = test2[3] / np.timedelta64(1, 'm')
+    res['Total_time_max'] = test2[7] / np.timedelta64(1, 'm')
+    res['Total_time_25'] = test2[4] / np.timedelta64(1, 'm')
+    res['Total_time_50'] = test2[5] / np.timedelta64(1, 'm')
+    res['Total_time_75'] = test2[6] / np.timedelta64(1, 'm')
+    res['car_type'] = name[0]
+    fecha = name[2].split('-')
+    a = datetime(int(fecha[0]), int(fecha[1]), 1)
+    a = datetime.timestamp(a)
+    res['timestamp'] = a
+
+    client = get_mongo_client()
+    # Issue the serverStatus command and print the results
+    mydb = client["NEW_YORK_TAXI"]
+    mycol = mydb["month"]
+    mycol.insert_one(res)
+
     return jsonify("Saved")
-
-
 
 if __name__ == '__main__':
     #app.run(debug=True, host='0.0.0.0')
